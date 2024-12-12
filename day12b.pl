c03a3186efc5d4d4f0be2a9299c8b4e4c18b02fa
Ken Brazier <kbrazier@gmail.com>
2024-12-12 10:59:26 -0700

    Day 12

---
+++ day12b.pl
@@ -0,0 +1,102 @@
+#!/usr/bin/perl -w
+use strict;
+use Math::Vector::Real;
+
+my $sum = 0;
+my $DEBUG=0;
+my @matrix;
+my %area;
+my %perimiter;
+
+sub pos2xy {
+  (my $base) = @_;
+  return ( $base->[0], $base->[1] );
+}
+
+while(<>) {
+  chomp;
+  /^[A-Z]*$/ || die "Invalid input $_";
+  push @matrix, [ '.', split(//), '.' ];
+}
+my $xsize = $#{$matrix[0]};
+my @vsentinel = split(//, '.'x($#{$matrix[0]}+1));
+push @matrix, \@vsentinel;
+unshift @matrix, \@vsentinel;
+
+# First, flood fill these regions with numbers instead so they're all unique.
+my $fillto = 0;
+my %fillmap;
+for(my $y=1; $y < $#matrix; $y++) {
+  for(my $x=1; $x < $xsize; $x++) {
+    if($matrix[$y][$x] =~ /^[A-Z]$/) {
+      my $fillfrom = $matrix[$y][$x];
+      $fillmap{$fillto} = $fillfrom;
+      my @filling = ( V($x, $y) );
+      my $fillcount = 0;
+      while($#filling >= 0) {
+        my $pos = shift(@filling);
+        (my $xf, my $yf) = pos2xy($pos);
+        if($matrix[$yf][$xf] eq $fillfrom) {
+          $matrix[$yf][$xf] = $fillto;
+          $fillcount++;
+          for(my $d = -1; $d <= 1; $d++) {
+            if($matrix[$yf][$xf+$d] eq $fillfrom) {
+              push @filling, V($xf+$d, $yf);
+            }
+            if($matrix[$yf+$d][$xf] eq $fillfrom) {
+              push @filling, V($xf, $yf+$d);
+            }
+          }
+        }
+      }
+      $DEBUG && print "Filled a region of $fillcount $fillfrom plants with $fillto\n";
+      $fillto++;
+    }
+  }
+}
+
+# Sentinels should be numbers too: -1.
+for(my $y=1; $y < $#matrix; $y++) {
+  $matrix[$y][0] = -1;
+  $matrix[$y][$xsize] = -1;
+}
+for(my $x=0; $x <= $xsize; $x++) {
+  $vsentinel[$x] = -1;
+}
+
+# Find areas and boundaries.
+# Yes, including the first sentinel, but not all the way to the last sentinel.
+for(my $y=0; $y < $#matrix; $y++) {
+  my $lasty0 = -1;
+  my $lasty1 = -1;
+  for(my $x=0; $x < $xsize; $x++) {
+    ++$area{$matrix[$y][$x]};
+    if($matrix[$y][$x] != $matrix[$y+1][$x] && ($matrix[$y][$x] != $lasty0 || $matrix[$y+1][$x] != $lasty1)) {
+      $perimiter{$matrix[$y][$x]}++ unless($matrix[$y][$x] == $lasty0 && $matrix[$y][$x] != $lasty1);
+      $perimiter{$matrix[$y+1][$x]}++ unless($matrix[$y+1][$x] == $lasty1 && $matrix[$y+1][$x] != $lasty0);
+    }
+    $lasty0 = $matrix[$y][$x];
+    $lasty1 = $matrix[$y+1][$x];
+  }
+}
+for(my $x=0; $x < $xsize; $x++) {
+  my $lastx0 = -1;
+  my $lastx1 = -1;
+  for(my $y=0; $y < $#matrix; $y++) {
+    if($matrix[$y][$x] != $matrix[$y][$x+1] && ($matrix[$y][$x] != $lastx0 || $matrix[$y][$x+1] != $lastx1)) {
+      $perimiter{$matrix[$y][$x]}++ unless($matrix[$y][$x] == $lastx0 && $matrix[$y][$x] != $lastx1);
+      $perimiter{$matrix[$y][$x+1]}++ unless($matrix[$y][$x+1] == $lastx1 && $matrix[$y][$x+1] != $lastx0);
+    }
+    $lastx0 = $matrix[$y][$x];
+    $lastx1 = $matrix[$y][$x+1];
+  }
+}
+
+foreach my $plot (sort keys(%area)) {
+  next if $plot < 0;
+  my $prod = $area{$plot} * $perimiter{$plot};
+  $DEBUG && print "A region #$plot of $fillmap{$plot} plants with price $area{$plot} * $perimiter{$plot} = $prod.\n";
+  $sum += $prod;
+}
+
+print $sum, "\n";
